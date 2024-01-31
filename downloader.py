97dd6adef69bcd990c228f1ba902ca7a069541ea
CJayM <cjaym@yandex.ru>
2024-01-31 22:25:19 +0300

    Добавлено сохранение в пикл индекса последнего скаченного тайла и зума

--- downloader.py
+++ downloader.py
@@ -3,8 +3,26 @@ from sqlite3 import Error
 
 import asyncio
 import aiohttp
+import pickle
+import os
 
 conn = None
+
+DB_FILE = "tiles.db3"
+pickle_lock = asyncio.Lock()
+db_lock = asyncio.Lock()
+
+
+class Settings:
+    FILE_NAME = "settings.pickle"
+
+    def __init__(self):
+        self.current_zoom = 1
+        self.current_cell = -1
+        self.buffered_cells = set()
+
+
+SETTINGS = Settings()
 
 
 def create_table(conn, zoom):
@@ -26,13 +44,45 @@ def create_table(conn, zoom):
         print(e)
 
 
-def save_in_db(x, y, zoom, data, percent):
+async def save_in_db(x, y, zoom, data, percent):
     task_1 = (x, y, data, 'png')
     sql = f'INSERT OR IGNORE INTO z{zoom}(x,y,image, ext) VALUES(?,?,?,?)'
-    cur = conn.cursor()
-    cur.execute(sql, task_1)
-    conn.commit()
-    print(f"[{percent:.2f}%]    Saved [{zoom}]:{x}x{y} ")
+
+    async with db_lock:
+        cur = conn.cursor()
+        cur.execute(sql, task_1)
+        conn.commit()
+
+
+async def save_in_pickle(x, y, zoom):
+    if zoom < SETTINGS.current_zoom:
+        return
+
+    size = 2 ** zoom
+    index = y * size + x
+
+    if index < SETTINGS.current_cell:
+        return
+
+    async with pickle_lock:
+        if SETTINGS.current_cell == index - 1:
+            SETTINGS.current_cell = index
+        else:
+            SETTINGS.buffered_cells.add(index)
+
+        if SETTINGS.buffered_cells:
+            buffered = list(SETTINGS.buffered_cells)
+            while buffered and buffered[0] == SETTINGS.current_cell + 1:
+                SETTINGS.current_cell += 1
+                del buffered[0]
+                SETTINGS.buffered_cells = set(buffered)
+
+        if SETTINGS.current_cell + 1 == size * size:
+            SETTINGS.current_cell = -1
+            SETTINGS.current_zoom += 1
+
+        with open(Settings.FILE_NAME, 'wb') as file:
+            pickle.dump(SETTINGS, file)
 
 
 def is_tile_exsists(x, y, zoom):
@@ -51,7 +101,9 @@ async def download_tile(x, y, zoom, percent):
             content = await resp.read()
 
             try:
-                save_in_db(x, y, zoom, content, percent)
+                await save_in_db(x, y, zoom, content, percent)
+                await save_in_pickle(x, y, zoom)
+                print(f"[{percent:.2f}%]    Saved [{zoom}]:{x}x{y} ")
             except Error as e:
                 print(e)
 
@@ -72,6 +124,7 @@ async def download(zoom):
             current += 1
 
             if is_tile_exsists(x, y, zoom):
+                save_in_pickle(x, y, zoom)
                 continue
 
             percent = current / total * 100.0
@@ -85,15 +138,15 @@ async def download(zoom):
 
 
 if __name__ == "__main__":
-    db_file = "World.db3"
-
-    zoom = 7
+    if os.path.exists(Settings.FILE_NAME):
+        with open(Settings.FILE_NAME, 'rb') as file:
+            SETTINGS = pickle.load(file)
 
     try:
-        conn = sqlite3.connect(db_file)
-        create_table(conn, zoom)
+        conn = sqlite3.connect(DB_FILE)
+        create_table(conn, SETTINGS.current_zoom)
 
-        asyncio.run(download(zoom))
+        asyncio.run(download(SETTINGS.current_zoom))
     except Error as e:
         print(e)
 
